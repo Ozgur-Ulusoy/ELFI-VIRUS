8332371e347c5912ba30051a06131f0589d64f68
Özgür <88507880+Ozgur-Ulusoy@users.noreply.github.com>
2024-10-10 21:03:04 +0300

    initalized project

---
+++ src/main/java/com/isee/elfi/ElfiApplication.java
@@ -0,0 +1,13 @@
+package com.isee.elfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElfiApplication.class, args);
+	}
+
+}
